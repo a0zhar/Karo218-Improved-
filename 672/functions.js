e42e904733548ba007e87268f8992d8be0ffdb94
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-02-10 19:44:06 +0100

    Major Changes

---
+++ 672/functions.js
@@ -0,0 +1,110 @@
+window.addEventListener("load", () => {
+  const cacheUPDtxt = document.getElementById("cacheUPDtxt");
+  const CacheBar = document.getElementById("CacheBar");
+  const passCounter = document.getElementById("passCounter");
+  const failCounter = document.getElementById("failCounter");
+
+  // Check if the page has already been cached
+  if (!localStorage.iscached || typeof localStorage.iscached === "undefined") {
+    // Prompt the user to cache the page for offline usage
+    if (window.confirm("Do you want to cache this page, for offline use?")) {
+      // Show the caching progress
+      window.applicationCache.addEventListener("progress", (e) => {
+        cacheUPDtxt.innerHTML = `Installing Offline Cache: ${Math.round(
+          (e.loaded / e.total) * 100
+        )}%`;
+        CacheBar.style.width = `${Math.round((e.loaded / e.total) * 100)}%`;
+      });
+
+      window.applicationCache.addEventListener("cached", () => {
+        // Show the cache is successfully installed and reload the page
+        cacheUPDtxt.innerHTML = "Page Cached successfully, reloading....";
+        window.location.reload();
+      });
+
+      window.applicationCache.addEventListener("updateready", () => {
+        // Show the cache is successfully installed and reload the page
+        cacheUPDtxt.innerHTML = "Page Cached successfully, reloading....";
+        window.location.reload();
+      });
+      // Show an error message if there was an error installing the cache
+      window.applicationCache.addEventListener("error", () => {
+        cacheUPDtxt.innerHTML = "Error Installing Cache";
+      });
+      // Set this to true, so that it won't be prompted again unless the website data is cleared on PS4 console
+      localStorage.iscached = true;
+    }
+  }
+
+  // Initialize the passcount and failcount in localStorage
+  localStorage.passcount = localStorage.passcount || 0;
+  localStorage.failcount = localStorage.failcount || 0;
+
+  // Update the pass and fail counters on the page
+  passCounter.innerHTML = localStorage.passcount;
+  failCounter.innerHTML = localStorage.failcount;
+
+  setupEventListeners();
+  setupDateElements();
+});
+function setupDateElements() {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  document.getElementById("date").innerHTML = `${day}/${month}/${year}`;
+
+  setInterval(() => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    document.getElementById("clock").innerHTML = `${
+      hours < 10 ? "0" + hours : hours
+    }:${minutes < 10 ? "0" + minutes : minutes}`;
+  }, 1000);
+}
+
+function setupEventListeners() {
+  // The parent node, element with id of MyItems
+  const parentElement = document.getElementById("MyItems");
+  // Selects All Elements with the pl-info attribute added
+  const buttons = parentElement.querySelectorAll("button[pl-info]");
+  // loops trough all of them
+  buttons.forEach((button) => {
+    // When cursor is on one of the payload buttons, show its info.
+    button.addEventListener("mouseover", () => {
+      msgs2.innerHTML = button.getAttribute("pl-info");
+    });
+    // When cursor isn't on the button anymore, reset msgs2 text.
+    button.addEventListener("mouseout", () => {
+      msgs2.innerHTML = "waiting for payload selection";
+    });
+  });
+}
+
+function addScript(src) {
+  const head = document.getElementsByTagName("head")[0];
+  const existingScript = document.getElementById("expload");
+
+  // If it has already been added before...
+  if (existingScript) head.removeChild(existingScript);
+
+  const script = document.createElement("script");
+  script.src = src;
+  script.id = "expload";
+  head.appendChild(script);
+}
+
+function loadBinFile(element) {
+  LoadedMSG = element.getAttribute("pl-loadedmsg");
+  PLfile = `bin/${element.getAttribute("pl-file")}`;
+
+  if (PLfile.includes("hen")) {
+    document.getElementById("msgs2").innerHTML = "Jailbreaking, please wait";
+    setTimeout(jailbreak, 500);
+  }
+
+  setTimeout(() => {
+    addScript("../common/loader.js");
+  }, 2000);
+}
