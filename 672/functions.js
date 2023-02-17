3d7e26a5291d067bf8da7b8cfd71ebe968b5601e
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-02-17 18:33:29 +0100

    Update functions.js

--- 672/functions.js
+++ 672/functions.js
@@ -4,38 +4,38 @@ localStorage.failcount = localStorage.failcount || 0;
 
 function setupDateElements() {
   const date = new Date();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
-  document.getElementById("date").textContent = `${day}/${month}/${year}`;
+  document.getElementById("date").innerHTML = `${day}/${month}/${year}`;
 
-  const updateClock = () => {
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-    document.getElementById("clock").textContent = `${hours}:${minutes}`;
-    setTimeout(updateClock, 1000);
-  };
+  setTimeout(function () {
+    let hours = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+    document.getElementById("clock").innerHTML = `${hours}:${minutes}`;
+  }, 1000);
 
   updateClock();
 }
 
 function setupEventListeners() {
   // The parent node, element with id of MyItems
-  const parentElement = document.getElementById("MyItems");
   // Selects All Elements with the pl-info attribute added
-  const buttons = parentElement.querySelectorAll("button[pl-info]");
   // loops trough all of them
-  buttons.forEach((button) => {
-    // When cursor is on one of the payload buttons, show its info.
-    button.addEventListener("mouseover", () => {
-      msgs2.textContent = button.getAttribute("pl-info");
+  document
+    .getElementById("MyItems")
+    .querySelectorAll("[pl-info]")
+    .forEach((plElm) => {
+      // When cursor is on one of the payload buttons, show its info.
+      plElm.addEventListener("mouseover", () => {
+        msgs2.innerHTML = plElm.getAttribute("pl-info");
+      });
+      // When cursor isn't on the button anymore, reset msgs2 text.
+      plElm.addEventListener("mouseout", () => {
+        msgs2.innerHTML = "waiting for payload selection";
+      });
     });
-    // When cursor isn't on the button anymore, reset msgs2 text.
-    button.addEventListener("mouseout", () => {
-      msgs2.textContent = "waiting for payload selection";
-    });
-  });
 }
 
 setupEventListeners();
@@ -45,27 +45,27 @@ setupDateElements();
 window.applicationCache.addEventListener("progress", ({ loaded, total }) => {
   let progress = Math.round((loaded / total) * 100);
   ScrOverlay.style.display = "block";
-  cacheUPDtxt.textContent = `Installing Offline Cache: ${progress}%`;
+  cacheUPDtxt.innerHTML = `Installing Offline Cache: ${progress}%`;
   CacheBar.style.width = `${progress}%`;
 });
 window.applicationCache.addEventListener("cached", () => {
-  cacheUPDtxt.textContent = "Page is Cached";
+  cacheUPDtxt.innerHTML = "Page is Cached";
 });
 window.applicationCache.addEventListener("updateready", () => {
-  cacheUPDtxt.textContent = "Page is Cached";
+  cacheUPDtxt.innerHTML = "Page is Cached";
 });
 window.applicationCache.addEventListener("error", () => {
   cacheUPDtxt.innerHTML = "Error Installing Cache";
 });
 
 function addScript(src) {
-  const head = document.getElementsByTagName("head")[0];
-  const existingScript = document.getElementById("expload");
+  let head = document.getElementsByTagName("head")[0];
+  let existingScript = document.getElementById("expload");
 
   // If it has already been added before...
   if (existingScript) head.removeChild(existingScript);
 
-  const script = document.createElement("script");
+  let script = document.createElement("script");
   script.src = src;
   script.id = "expload";
   head.appendChild(script);
